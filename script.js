86d411b65b90808fa376e3bf2d4bdd9cb25abb62
brunasantana395 <bruna.delphino.santana@escola.pr.gov.br>
2025-08-06 13:27:01 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-unction inserirNome(){
+function inserirNome(){
 let nomeUsuario = prompt("Qual o seu nome");
    let elemento = document.querySelector("#nome-usuario");
    console.log(elemento);
@@ -12,5 +12,7 @@ lista.textContent = linguagens[1];
 lista.textContent = linguagens[2];
 
 let aluno1 = {
-    
-}
+   nome: `bruna',
+}  idade:18, 
+   anoletivo: '3° ensino médio',
+   Materiafavoritas:['historia']
